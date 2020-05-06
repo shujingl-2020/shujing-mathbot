7369d6df7a90caf0e3edd7609473f08fda9725fc
shujingl-2020 <62564698+shujingl-2020@users.noreply.github.com>
2020-05-06 19:09:30 -0400

    hi

--- app.rb
+++ app.rb
@@ -365,7 +365,7 @@ end
 def get_gif_for query
   human_greetngs = ["hi","what's up","hello","hi there","what can you do"]
   if match query, human_greetngs
-		query = "hello"
+		query = "welcome"
 	end
   Giphy::Configuration.configure do |config|
     config.api_key = ENV["GIPHY_API_KEY"]
